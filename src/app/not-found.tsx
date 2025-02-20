f39bdb172342c1af22abef7a8d8c4664befa127d
Christophe Rosset <tophe@topheman.com>
2025-02-20 12:56:59 +0100

    refactor: move not-fount.tsx at the root

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,9 @@
 export default function NotFound() {
   return (
-    <div className="flex min-h-[50vh] flex-col items-center justify-center">
+    <div
+      className="flex min-h-[50vh] flex-col items-center justify-center"
+      id="not-found"
+    >
       <h2 className="mb-4 text-2xl font-bold">404 - Page Not Found</h2>
       <p>The page you are looking for does not exist.</p>
     </div>
