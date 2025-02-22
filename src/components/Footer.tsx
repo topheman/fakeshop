3303053bd031db04e4ab0625296dd098fd66a725
Christophe Rosset <tophe@topheman.com>
2025-02-22 16:48:45 +0100

    feat: update link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,30 +4,11 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 p-4 text-primary">
       <div className="container mx-auto text-center">
-        <p>&copy; 2025 - Christophe Rosset</p>
-        <div className="mt-2 space-x-4">
-          <Link
-            href="https://github.com/topheman"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
+        <p>
+          <Link href="https://topheman.github.io/me/" target="_blank">
+            &copy; 2025 - Christophe Rosset
           </Link>
-          <Link
-            href="https://www.linkedin.com/in/topheman/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </Link>
-          <Link
-            href="https://twitter.com/topheman"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </Link>
-        </div>
+        </p>
       </div>
     </footer>
   );
