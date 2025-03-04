0634b4df1c467ba1031fe6cae483dbc4940b1c7e
Christophe Rosset <tophe@topheman.com>
2025-03-04 11:24:04 +0100

    refactor: remove unecessary CartProvider

--- src/lib/query/provider.tsx
+++ src/lib/query/provider.tsx
@@ -4,15 +4,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
-import { CartProvider } from "@/hooks/cart";
-
 import { makeQueryClient } from "./client";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => makeQueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <CartProvider>{children}</CartProvider>
+      {children}
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
